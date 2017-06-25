c2cfa5e4d151868a3313cb8b2ad72df0a96498a5
geophydog <geophydogvon@gmail.com>
2017-06-25 21:05:10 +0800

    modified

--- time-fre.c
+++ time-fre.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
     fprintf(fp,"grdimage %s.grd -R -J -K -O -Ctmp.cpt -B%d:\"Time(sec)\":/%f:\"Frequency(Hz)\":WSen>>plot.ps\n", argv[4], (int)((e-b)/10.), high_f/10.);
     fprintf(fp,"echo %s 0 1 | pssac2 -R%f/%f/0/2 -JX8i/1i -K -O -B%d/1:\"amp\":WSen -C%f/%f -M0.8 -W0.8p -Xa0i -Ya5.4i>>plot.ps\n", argv[1], b, e, (int)((e-b)/10.), b, e);
     fprintf(fp,"psscale -Ctmp1.cpt -D8.5i/2.5i/12.55/0.8 -Ba0.1g0:\"Normalized amplitude power\": -K -O >>plot.ps\n");
-    fprintf(fp,"echo %.1f 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>plot.ps\n", hd.e/2, argv[1]);
+    fprintf(fp,"echo %.1f 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>plot.ps\n", (b+e)/2., argv[1]);
     fprintf(fp,"psxy -R -J -O -T >>plot.ps\n");
     fprintf(fp,"ps2pdf plot.ps plot.pdf\n");
     fprintf(fp,"rm tmp*.cpt\n");
