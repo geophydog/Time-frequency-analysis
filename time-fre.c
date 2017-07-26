289e711b797a80bd72794ecb1b5e54fc374b9e83
geophydog <geophydogvon@gmail.com>
2017-07-26 21:27:23 +0800

    modified

--- time-fre.c
+++ time-fre.c
@@ -14,20 +14,21 @@ int near_pow2(int n) {
 
 int main(int argc, char *argv[]) {
     int i, k, npts, seg_npts, seg_num, begin_index;
-    float seg_len, *data, t1, t2, time = 0., fre, amp, high_f, peak = 0.;
+    float seg_len, *data, t1, t2, time = 0., fre, amp, low_f, high_f, peak = 0.;
     SACHEAD hd;
     fftw_complex *in, *out;
     fftw_plan p;
     FILE *ft, *fp;
 
-    if( argc != 7 ) {
+    if( argc != 8 ) {
         fprintf(stderr,"Usage: time-fre <sacfile> <t1> <t2> <segmentation_length> <high_frequency_limit> <output_result_file_name>\n");
         fprintf(stderr,"       return power-spectral in time-frequency domains,which will be saved output_result_file_name\n");
         fprintf(stderr,"       <sacfile>                 The name of inputing SAC format file;\n");
         fprintf(stderr,"       <t1>                      The beginning time of time-fre analysis;\n");
         fprintf(stderr,"       <t2>                      The ending time of time-fre analysis;\n");
         fprintf(stderr,"       <segmentation_length>     The short FFT time length;\n");
-        fprintf(stderr,"       <high_frerquency_limit>   The maximum of frequency of outputing power spectral;\n");
+        fprintf(stderr,"       <low_f>                   The low corner frequency;\n");
+        fprintf(stderr,"       <high_frerquency_limit>   The high corner frequency;\n");
         fprintf(stderr,"       <output_result_file_name> The name of outputing results;\n");
         fprintf(stderr,"         Power spectral plot by will be saved in file \"plot.sh\",just run with command \"sh plot.sh\".\n");
         fprintf(stderr,"         Attention!!! Executing \"sh plot.sh\" requires GMT(the Generic Mapping Tools).\n");
@@ -37,8 +38,9 @@ int main(int argc, char *argv[]) {
     t1 = atof(argv[2]);
     t2 = atof(argv[3]);
     seg_len= atof(argv[4]);
-    high_f = atof(argv[5]);
-    ft = fopen(argv[6],"w");
+    low_f = atof(argv[5]);
+    high_f = atof(argv[6]);
+    ft = fopen(argv[7],"w");
     fp = fopen("plot.sh","w");
 
     data = read_sac(argv[1],&hd);
@@ -76,22 +78,22 @@ int main(int argc, char *argv[]) {
 
     fprintf(fp,"gmtset LABEL_FONT Times-Bold\n");
     fprintf(fp,"gmtset LABEL_FONT_SIZE 15p\n");
-    fprintf(fp,"awk '{print $1,$2,$3/%f}' %s >tmp.file\n", peak, argv[6]);
-    fprintf(fp,"rm %s\n", argv[6]);
-    fprintf(fp,"mv tmp.file %s\n", argv[6]);
+    fprintf(fp,"awk '{print $1,$2,$3/%f}' %s >tmp.file\n", peak, argv[7]);
+    fprintf(fp,"rm %s\n", argv[7]);
+    fprintf(fp,"mv tmp.file %s\n", argv[7]);
     fprintf(fp,"makecpt -Cjet.cpt -T0/1.0/0.1 -Z >tmp1.cpt\n");
-    fprintf(fp,"psxy -R%f/%.1f/0/%f -JX8i/5i -K -T >plot.ps\n", t1, t2, high_f);
-    fprintf(fp,"surface %s -R -I%d/%f -G%s.grd\n" ,argv[6], (int)(seg_len/2), high_f/100., argv[6]);
-    fprintf(fp,"grd2cpt %s.grd -Cjet>tmp.cpt\n",argv[6]);
-    fprintf(fp,"grdimage %s.grd -R -J -K -O -Ctmp.cpt -B%d:\"Time(sec)\":/%f:\"Frequency(Hz)\":WSen>>plot.ps\n", argv[6], (int)((t2-t1)/10.), high_f/10.);
-    fprintf(fp,"echo %s 0 1 | pssac2 -R%f/%f/0/2 -JX8i/1i -K -O -B%d/0:\"Amplitude\":WSen -C%f/%f -M0.8 -W0.8p -Xa0i -Ya5.4i>>plot.ps\n", argv[1], t1, t2,(int)((t2-t1)/10), t1, t2);
-    fprintf(fp,"psscale -Ctmp1.cpt -D8.5i/2.5i/12.55/0.8 -Ba0.1g0:\"Normalized amplitude power\": -K -O >>plot.ps\n");
-    fprintf(fp,"echo %.1f 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>plot.ps\n", (t1+t2)/2., argv[1]);
-    fprintf(fp,"psxy -R -J -O -T >>plot.ps\n");
-    fprintf(fp,"gmt psconvert -Tg -A -P plot.ps\n");
-    fprintf(fp,"ps2pdf plot.ps plot.pdf\n");
+    fprintf(fp,"psxy -R%f/%.1f/%f/%f -JX8i/5i -K -T >%s.ps\n", t1, t2, low_f, high_f, argv[1]);
+    fprintf(fp,"surface %s -R -I%f/%f -G%s.grd\n" ,argv[7], seg_len/2, (high_f-low_f)/100., argv[7]);
+    fprintf(fp,"grd2cpt %s.grd -Cjet>tmp.cpt\n",argv[7]);
+    fprintf(fp,"grdimage %s.grd -R -J -K -O -Ctmp.cpt -B%d:\"Time(sec)\":/%f:\"Frequency(Hz)\":WSen>>%s.ps\n", argv[7], (int)((t2-t1)/10.), high_f/10., argv[1]);
+    fprintf(fp,"echo %s 0 1 | pssac2 -R%f/%f/0/2 -JX8i/1i -K -O -B%d/0:\"Amplitude\":WSen -C%f/%f -M0.8 -W0.8p -Xa0i -Ya5.4i>>%s.ps\n", argv[1], t1, t2,(int)((t2-t1)/10), t1, t2, argv[1]);
+    fprintf(fp,"psscale -Ctmp1.cpt -D8.5i/2.5i/12.55/0.8 -Ba0.1g0:\"Normalized amplitude power\": -K -O >>%s.ps\n", argv[1]);
+    fprintf(fp,"echo %.1f 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>%s.ps\n", (t1+t2)/2., argv[1], argv[1]);
+    fprintf(fp,"psxy -R -J -O -T >>%s.ps\n", argv[1]);
+    fprintf(fp,"gmt psconvert -Tg -A -P %s.ps\n", argv[1]);
+    fprintf(fp,"ps2pdf %s.ps %s.pdf\n", argv[1], argv[1]);
     fprintf(fp,"rm tmp*.cpt\n");
-    fprintf(fp,"evince plot.pdf\n");
+    fprintf(fp,"evince %s.pdf\n", argv[1]);
     fclose(fp);
 
     free(data);
