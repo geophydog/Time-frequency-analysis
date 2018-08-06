88226b228ebc65872bd47120119bd1506abb2056
Xuping Feng <2514037718@qq.com>
2018-08-06 21:27:33 +0800

    modified resolution of "surface" command

--- src/time-fre.c
+++ src/time-fre.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <fftw3.h>
+#include <string.h>
 #include "sacio.h"
 
-
-// To get an integer m greater or equal to integer n. m >= n; m = 2^k, k is an integer and k > 0.
 int near_pow2(int n) {
     int m;
     float f;
@@ -14,7 +13,6 @@ int near_pow2(int n) {
     return m;
 }
 
-// Here is the main program.
 int main(int argc, char *argv[]) {
     int i, k, npts, seg_npts, seg_num, begin_index;
     float seg_len, *data, t1, t2, time = 0., fre, amp, low_f, high_f, peak = 0.;
@@ -22,8 +20,8 @@ int main(int argc, char *argv[]) {
     fftw_complex *in, *out;
     fftw_plan p;
     FILE *ft, *fp;
+    char str[256] = {""};
 
-// Check parameters and print the usage if parameters are not enough.
     if( argc != 8 ) {
         fprintf(stderr,"Usage: time-fre <sacfile> <t1> <t2> <segmentation_length> <low_frequency> <high_frequency> <output_result_file_name>\n");
         fprintf(stderr,"       return power-spectral in time-frequency domains,which will be saved output_result_file_name\n");
@@ -39,7 +37,6 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-// Get parameters inputted.
     t1 = atof(argv[2]);
     t2 = atof(argv[3]);
     seg_len= atof(argv[4]);
@@ -48,14 +45,12 @@ int main(int argc, char *argv[]) {
     ft = fopen(argv[7],"w");
     fp = fopen("plot.sh","w");
 
- // Reading into SAC format file.
     data = read_sac(argv[1],&hd);
     seg_num = (int)((t2-t1)/seg_len);
     seg_npts = (int)(seg_len/hd.delta);
     begin_index = (int)((t1-hd.b)/hd.delta);
     npts = near_pow2(seg_npts);
 
-// Declare two complex types: "in" and "out".
     in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * npts);
     out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * npts);
 
@@ -83,23 +78,23 @@ int main(int argc, char *argv[]) {
     }
     fftw_free(in); fftw_free(out);
 
-// Write plot script based on GMT (the Generic Mapping Tools) syntax.
-   fprintf(fp,"gmtset LABEL_FONT Times-Bold\n");
+//----------------------------------------------------shell plot script-------------------------------------------------------------
+    fprintf(fp,"gmtset LABEL_FONT Times-Bold\n");
     fprintf(fp,"gmtset LABEL_FONT_SIZE 20\n");
     fprintf(fp,"gmtset ANNOT_FONT_PRIMARY 5\ngmtset ANNOT_FONT_SIZE_PRIMARY 15\n");
-    fprintf(fp,"awk '{print $1,$2,$3/%f}' %s >tmp.file\n", peak, argv[7]);
+    fprintf(fp,"awk '{print $1,$2,$3/%g}' %s >tmp.file\n", peak, argv[7]);
     fprintf(fp,"makecpt -Cjet.cpt -T0/1.0/0.1 -Z >tmp1.cpt\n");
-    fprintf(fp,"psxy -R%f/%.1f/%f/%f -JX8i/5i -K -T >%s.ps\n", t1, t2, low_f, high_f, argv[1]);
-    fprintf(fp,"surface tmp.file -R -I%f/%f -Gtmp.grd\n", seg_len/4, (high_f-low_f)/100.);
+    fprintf(fp,"psxy -R%g/%.1g/%g/%g -JX8i/5i -K -T >%s.ps\n", t1, t2, low_f, high_f, argv[1]);
+    fprintf(fp,"surface tmp.file -R -I%g/%g -Gtmp.grd\n", (t2-t1)/3333, (high_f-low_f)/200);
     fprintf(fp,"grd2cpt tmp.grd -Cjet>tmp.cpt\n");
-    fprintf(fp,"grdimage tmp.grd -R -J -K -O -Ctmp.cpt -B%d:\"Time(sec)\":/%f:\"Frequency(Hz)\":WSen>>%s.ps\n", \
+    fprintf(fp,"grdimage tmp.grd -R -J -K -O -Ctmp.cpt -B%d:\"Time(sec)\":/%g:\"Frequency(Hz)\":WSen>>%s.ps\n", \
             (int)((t2-t1)/10.), high_f/10., argv[1]);
-    fprintf(fp,"echo %s 0 1 | pssac2 -R%f/%f/0/2 -JX8i/1i -K -O -B%d/0:\"Amplitude\":WSen -C%f/%f -M0.8 -W0.8p -Xa0i -Ya5.4i>>%s.ps\n", \
+    fprintf(fp,"echo %s 0 1 | pssac2 -R%g/%g/0/2 -JX8i/1i -K -O -B%d/0:\"Amplitude\":WSen -C%g/%g -M0.8 -W0.8p -Xa0i -Ya5.4i>>%s.ps\n", \
             argv[1], t1, t2,(int)((t2-t1)/10), t1, t2, argv[1]);
-    fprintf(fp,"psscale -Ctmp1.cpt -D8.5i/2.5i/12.55/0.8 -Ba0.1g0:\"Normalized amplitude power\": -K -O >>%s.ps\n", argv[1]);
-    fprintf(fp,"echo %.1f 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>%s.ps\n", (t1+t2)/2., argv[1], argv[1]);
+    fprintf(fp,"psscale -Ctmp1.cpt -D8.5i/2.5i/12.55/0.6 -Ba0.1g0:\"Normalized amplitude power\": -K -O >>%s.ps\n", argv[1]);
+    fprintf(fp,"echo %g 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>%s.ps\n", (t1+t2)/2., argv[1], argv[1]);
     fprintf(fp,"psxy -R -J -O -T >>%s.ps\n", argv[1]);
-    fprintf(fp,"gmt psconvert -Tg -A -P %s.ps\n", argv[1]);
+    fprintf(fp,"ps2raster -Tt -A -P -E300 %s.ps\n", argv[1]);
     fprintf(fp,"ps2pdf %s.ps %s.pdf\n", argv[1], argv[1]);
     fprintf(fp,"rm tmp*\n");
     fprintf(fp,"evince %s.pdf\n", argv[1]);
