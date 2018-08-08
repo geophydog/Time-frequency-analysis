1434bf7c90ba5b50d35a972e66f21459b5168ebd
Xuping Feng <2514037718@qq.com>
2018-08-08 16:11:01 +0800

    Update time-fre.c

--- src/time-fre.c
+++ src/time-fre.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
     FILE *ft, *fp;
     char str[256] = {""};
 
+// Usage of program
     if( argc != 8 ) {
         fprintf(stderr,"Usage: time-fre <sacfile> <t1> <t2> <segmentation_length> <low_frequency> <high_frequency> <output_result_file_name>\n");
         fprintf(stderr,"       return power-spectral in time-frequency domains,which will be saved output_result_file_name\n");
@@ -35,7 +36,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr,"       <<<Attention!!! Executing \"sh plot.sh\" requires GMT(the Generic Mapping Tools).\n");
         exit(1);
     }
-
+// get parameters input.
     t1 = atof(argv[2]);
     t2 = atof(argv[3]);
     seg_len= atof(argv[4]);
@@ -44,25 +45,27 @@ int main(int argc, char *argv[]) {
     ft = fopen(argv[7],"w");
     fp = fopen("plot.sh","w");
 
+// to verify your high corner frequency is more than Nyquist sampling frequency.
     data = read_sac(argv[1],&hd);
     sp = 1/hd.delta;
     if ( f2 >= sp/2 ) {
         f2 = sp/2*0.99;
-        fprintf(stderr, "ATTENTION!!! %g(your high_fre) >= %g(sp)\n", f2, sp);
+        fprintf(stderr, "ATTENTION!!! %g(your high_fre) >= %g(Nyquist sampling frequency)\n", f2, sp);
     }
 
+// get counts of segmentation of time, points of every segmentaion of time and begin and end index of frequency
     seg_num = (int)((t2-t1)/seg_len);
     seg_npts = (int)(seg_len/hd.delta);
     begin_index = (int)((t1-hd.b)/hd.delta);
     npts = near_pow2(seg_npts);
     f1_i = (int) (f1*npts/sp);
     f2_i = (int) (f2*npts/sp);
-    printf("fft npts: %d low_f: %g  high_f: %g  high_f_i: %d   low_f_i: %d  t1: %g  t2: %g seg_len: %g\n", \
-            npts, f1, f2, f1_i, f2_i, t1, t2, seg_len);
 
+// allocate dynamic memory of FFTW
     in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * npts);
     out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * npts);
 
+// main part of program
     while ( k <= seg_num ) {
         time = t1 + k*seg_len + seg_len/2.;
 
@@ -89,25 +92,26 @@ int main(int argc, char *argv[]) {
     fftw_free(in); fftw_free(out);
 
 //----------------------------------------------------shell plot script-------------------------------------------------------------
+    fprintf(fp,"R=%g/%g/%g/%g\nPS=%s.ps\nPDF=%s.pdf\n\n", t1, t2, f1, f2, argv[1]);
     fprintf(fp,"gmtset LABEL_FONT Times-Bold\n");
-    fprintf(fp,"gmtset LABEL_FONT_SIZE 20\n");
-    fprintf(fp,"gmtset ANNOT_FONT_PRIMARY 5\ngmtset ANNOT_FONT_SIZE_PRIMARY 15\n");
+    fprintf(fp,"gmtset LABEL_FONT_SIZE 18\n");
+    fprintf(fp,"gmtset ANNOT_FONT_PRIMARY 5\ngmtset ANNOT_FONT_SIZE_PRIMARY 15\n\n");
     fprintf(fp,"awk '{print $1,$2,$3/%g}' %s >tmp.file\n", peak, argv[7]);
     fprintf(fp,"makecpt -Cjet.cpt -T0/1.0/0.1 -Z >tmp1.cpt\n");
-    fprintf(fp,"psxy -R%g/%g/%g/%g -JX8i/5i -K -T >%s.ps\n", t1, t2, f1, f2, argv[1]);
-    fprintf(fp,"surface tmp.file -R -I%g/%g -Gtmp.grd\n", (t2-t1)/3333, (f2-f1)/200);
-    fprintf(fp,"grd2cpt tmp.grd -Cjet>tmp.cpt\n");
-    fprintf(fp,"grdimage tmp.grd -R -J -K -O -Ctmp.cpt -B%d:\"Time(sec)\":/%g:\"Frequency(Hz)\":WSen>>%s.ps\n", \
-            (int)((t2-t1)/10.), (f2-f1)/10., argv[1]);
-    fprintf(fp,"echo %s 0 1 | pssac2 -R%g/%g/0/2 -JX8i/1i -K -O -B%d/0:\"Amplitude\":WSen -C%g/%g -M0.8 -W0.8p -Xa0i -Ya5.4i>>%s.ps\n", \
-            argv[1], t1, t2,(int)((t2-t1)/10), t1, t2, argv[1]);
-    fprintf(fp,"psscale -Ctmp1.cpt -D8.5i/2.5i/12.55/0.6 -Ba0.1g0:\"Normalized amplitude power\": -K -O >>%s.ps\n", argv[1]);
-    fprintf(fp,"echo %g 1 20 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa-1i -Ya6.2i>>%s.ps\n", (t1+t2)/2., argv[1], argv[1]);
-    fprintf(fp,"psxy -R -J -O -T >>%s.ps\n", argv[1]);
-    fprintf(fp,"ps2raster -Tt -A -P -E300 %s.ps\n", argv[1]);
-    fprintf(fp,"ps2pdf %s.ps %s.pdf\n", argv[1], argv[1]);
-    fprintf(fp,"rm tmp*\n");
-    fprintf(fp,"evince %s.pdf\n", argv[1]);
+    fprintf(fp,"surface tmp.file -R$R -I%g/%g -Gtmp.grd\n", (t2-t1)/3000, (f2-f1)/200);
+    fprintf(fp,"grd2cpt tmp.grd -Cjet>tmp.cpt\n\n");
+    fprintf(fp,"psxy -R$R -JX8i/4i -K -T > $PS\n");
+    fprintf(fp,"grdimage tmp.grd -R -J -K -O -Ctmp.cpt -B%g:\"Time(sec)\":/%g:\"Frequency(Hz)\":WSen -Xa1.5i -Ya1i >> $PS\n", \
+            (t2-t1)/10, (f2-f1)/10);
+    fprintf(fp,"echo %s 0 1 | pssac2 -R%g/%g/0/2 -JX8i/1i -K -O -B%d/0:\"Amplitude\":WSen -C%g/%g -M0.8 -W0.8p -Xa1.5i -Ya5.5i >> $PS\n", \
+            argv[1], t1, t2,(int)((t2-t1)/10), t1, t2);
+    fprintf(fp,"psscale -Ctmp1.cpt -D9.8i/3i/10.15/0.5 -Ba0.1g0:\"Normalized amplitude power\": -K -O >> $PS\n");
+    fprintf(fp,"echo %g 1 15 0 Times-Bold 0 %s | pstext -R -J -K -O -Xa0i -Ya6.2i>> $PS\n", (t1+t2)/2., argv[1]);
+    fprintf(fp,"psxy -R -J -O -T >> $PS\n\n");
+    fprintf(fp,"ps2raster -Tt -A -P -E300 $PS\n");
+    fprintf(fp,"ps2pdf $PS $PDF\n");
+    fprintf(fp,"rm tmp* %s* $PS\n", argv[7]);
+    fprintf(fp,"evince $PDF\n");
     fclose(fp);
 
     free(data);
